d653d1f70326baf3bdf857b90b8479e28d665e57
jbeyr <jameson@jbey.dev>
2025-08-02 23:56:25 -0500

    bump to 0.2

--- src/main/java/slate/Main.java
+++ src/main/java/slate/Main.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ScheduledExecutorService;
 public class Main
 {
     public static final String MODID = "slate";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
 
     public static Minecraft mc = Minecraft.getMinecraft();
     private static final ScheduledExecutorService ex = Executors.newScheduledThreadPool(4);
